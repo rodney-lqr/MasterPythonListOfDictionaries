390825a6abc00c6b4b9b66b7f3f44962ccc9a2fb
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-10-09 22:42:30 +0800

    Fourth Commit: Created a List of Dictionary about Universities

---
+++ universities.py
@@ -0,0 +1,12 @@
+# This is a list of university dictionaries
+universities = [
+    {"name": "Harvard", "location": "USA"},
+    {"name": "Stanford", "location": "USA"},
+    {"name": "MIT", "location": "USA"},
+    {"name": "Cambridge", "location": "UK"},
+    {"name": "Oxford", "location": "UK"}
+]
+
+# Print university data
+for university in universities:
+    print(f"University: {university['name']}, Location: {university['location']}")
