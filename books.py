b9b22f661a80c2b895514664bee8c24af04b1971
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-10-09 22:09:44 +0800

    First Initial Commit: Created a list of dictionaries about Books and prints them

---
+++ books.py
@@ -0,0 +1,12 @@
+# This is a list of book dictionaries
+books = [
+    {"title": "1984", "author": "George Orwell"},
+    {"title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
+    {"title": "Moby Dick", "author": "Herman Melville"},
+    {"title": "War and Peace", "author": "Leo Tolstoy"}
+]
+
+# Print book data
+for book in books:
+    print(f"Book: {book['title']}, Author: {book['author']}")
